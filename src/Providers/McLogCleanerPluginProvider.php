f54b580ee964cbd6a471cdaa3a4a88c7e79d352b
JuggleFredJr <mail@philippstamp.de>
2026-05-31 10:44:49 +0200

    Adding the delete-button to the files-tab

--- src/Providers/McLogCleanerPluginProvider.php
+++ src/Providers/McLogCleanerPluginProvider.php
@@ -4,6 +4,8 @@ namespace JuggleGaming\McLogCleaner\Providers;
 
 use App\Enums\HeaderActionPosition;
 use App\Filament\Server\Pages\Console;
+use App\Filament\Server\Resources\Files\Pages\EditFiles;
+use App\Filament\Server\Resources\Files\Pages\ListFiles;
 use Illuminate\Support\ServiceProvider;
 use JuggleGaming\McLogCleaner\Filament\Components\Actions\McLogCleanAction;
 
@@ -13,6 +15,7 @@ class McLogCleanerPluginProvider extends ServiceProvider
     {
         Console::registerCustomHeaderActions(HeaderActionPosition::Before, McLogCleanAction::make());
         EditFiles::registerCustomHeaderActions(HeaderActionPosition::Before, McLogCleanAction::make());
+        ListFiles::registerCustomHeaderActions(HeaderActionPosition::Before, McLogCleanAction::make());
     }
 
     public function boot(): void
