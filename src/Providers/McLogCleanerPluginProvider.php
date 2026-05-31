0130d36019476c2ad3d09d6db3e5d5fcfa6168b2
JuggleFredJr <mail@philippstamp.de>
2026-05-31 11:39:53 +0200

    Removing a forgotten test

--- src/Providers/McLogCleanerPluginProvider.php
+++ src/Providers/McLogCleanerPluginProvider.php
@@ -4,7 +4,6 @@ namespace JuggleGaming\McLogCleaner\Providers;
 
 use App\Enums\HeaderActionPosition;
 use App\Filament\Server\Pages\Console;
-use App\Filament\Server\Resources\Files\Pages\EditFiles;
 use App\Filament\Server\Resources\Files\Pages\ListFiles;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -15,7 +14,6 @@ class McLogCleanerPluginProvider extends ServiceProvider
     public function register(): void
     {
         Console::registerCustomHeaderActions(HeaderActionPosition::Before, McLogCleanAction::make());
-        EditFiles::registerCustomHeaderActions(HeaderActionPosition::Before, McLogCleanAction::make());
         ListFiles::registerCustomHeaderActions(
             HeaderActionPosition::Before,
             McLogCleanAction::make()
